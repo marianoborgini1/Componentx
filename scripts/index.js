c805d642a2e00826cef4f91abbc60b2ced6cc6e5
marianob <marianoborgini1@gmail.com>
2023-06-02 00:14:04 -0300

    guardado

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //Header Menu Hamburguesa//
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
     x.className += " responsive";
     } else {
@@ -8,4 +8,24 @@ function myFunction() {
     }
 }
 //Final Header//
+
+//Formulario alert lleno/vacio//
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Obtener los valores de los campos
+    let email = document.getElementById("email").value;
+    let celu = document.getElementById("number").value;
+    let dni = document.getElementById("dni").value;
+
+    // Verificar si los campos están vacíos
+    if (email === "" || celu === "" || dni === "") {
+    alert("El formulario está incompleto. Por favor, complete todos los campos.");
+    } else {
+    alert("Gracias por llenar el formulario, se comunicarán con ustedes a la brevedad.");
+    }
+});
+//Final Formulario alert lleno/vacio//
+
+
 
